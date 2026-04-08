a802b9d57e5b6e788d4fe3d9ab31f71ba4e8c524
kornel <gg@g.k>
2026-04-08 08:26:23 +0200

    naprawienie dzialania z baza, gety w koncu dzialaja

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,15 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import muminkiRoutes from "./routes/muminkiRoutes";
+import artefactsRoutes from "./routes/artefactsRoutes";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.use("/muminki", muminkiRoutes)
+app.use("/characters", muminkiRoutes)
+app.use("/artefacts", artefactsRoutes)
 
 app.get("/", (req, res) => {
     res.json({ message: "API Express + TypeScript działa!" });
